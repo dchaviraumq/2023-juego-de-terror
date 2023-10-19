ceb290e2c3968bd72004461f10dded560e097e45
Daniel Chavira <prof.d.chavira@umq.maristas.edu.mx>
2023-10-19 08:35:28 -0600

    Plugins

---
+++ Assets/Plugins/EditorTools/DevUtils/ScreenResolutionKeyboardShortcuts.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace EditorTools.DevUtils
+{
+    [Serializable]
+    public struct ScreenResolutionConfig
+    {
+        public KeyCode key;
+        public int     width;
+        public int     height;
+        public uint    refreshRate;
+
+        public RefreshRate RefreshRate => new() { numerator = refreshRate, denominator = 1 };
+    }
+
+    public class ScreenResolutionKeyboardShortcuts : MonoBehaviour
+    {
+        [SerializeField] private List<ScreenResolutionConfig> resolutions;
+
+        [SerializeField] private KeyCode fullScreenModeToggleKey;
+
+        [SerializeField] private bool checkForCtrl;
+        [SerializeField] private bool checkForAlt;
+
+        private void UpdateScreenResolution(ScreenResolutionConfig config) {
+            var screenMode = Screen.fullScreen ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed;
+
+            Screen.SetResolution(config.width, config.height, screenMode, config.RefreshRate);
+        }
+
+        private void UpdateFullScreenMode() {
+            Screen.fullScreen = !Screen.fullScreen;
+        }
+
+        private void Update() {
+            if (!ModifierPresent()) return;
+
+            if (Input.GetKeyDown(fullScreenModeToggleKey))
+            {
+                Debug.Log("Toggle full screen mode");
+                UpdateFullScreenMode();
+            }
+
+            foreach (var resolutionConfig in resolutions)
+            {
+                if (Input.GetKeyDown(resolutionConfig.key))
+                {
+                    Debug.Log("Resolution: " + resolutionConfig.width + "x" + resolutionConfig.height + " @ " +
+                              resolutionConfig.RefreshRate);
+                    UpdateScreenResolution(resolutionConfig);
+                }
+            }
+        }
+
+
+        private bool ModifierPresent() {
+            var ctrl = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
+            var alt  = Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt);
+
+            var ctrOk = !checkForCtrl || (checkForCtrl && ctrl);
+            var altOk = !checkForAlt || (checkForAlt && alt);
+
+            return ctrOk && altOk;
+        }
+    }
+}
---
+++ Assets/Plugins/EditorTools/Editor/FastReloadUtils.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace EditorTools.Editor
+{
+    public static class FastReloadUtils
+    {
+        private const string RED   = "#500000";
+        private const string GREEN = "#005000";
+
+        [MenuItem("Tools/Fast Reload/Toggle Fast Play Mode &#%f")]
+        public static void ToggleFastPlayMode() {
+            EditorSettings.enterPlayModeOptionsEnabled = !EditorSettings.enterPlayModeOptionsEnabled;
+
+            AssetDatabase.Refresh();
+
+            var color = EditorSettings.enterPlayModeOptionsEnabled ? GREEN : RED;
+            var text  = EditorSettings.enterPlayModeOptionsEnabled ? "Enabled" : "Disabled";
+
+            Debug.Log($"Fast Play Mode: <color={color}>{text}</color>");
+        }
+
+        [MenuItem("Tools/Fast Reload/Reload Domain &#%d")]
+        public static void ReloadDomain() {
+            EditorUtility.RequestScriptReload();
+            AssetDatabase.Refresh();
+
+            Debug.Log($"Reloading Domain: <color={GREEN}>DONE</color>");
+        }
+    }
+}
