54f6d19fa8fdea75c4701d7e8fa018e97e1289b6
Daniel Chavira <prof.d.chavira@umq.maristas.edu.mx>
2023-10-19 16:58:31 -0600

    Bloqueo de cursor

---
+++ Assets/Scripts/Misc/BloquearCursor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Scripts.Misc
+{
+    public class BloquearCursor : MonoBehaviour
+    {
+        [SerializeField] private KeyCode teclaDeBloqueo = KeyCode.Escape;
+
+        private void Awake() {
+            ModificarBloqueo();
+        }
+
+        private void Update() {
+            if (!Input.GetKeyDown(teclaDeBloqueo)) return;
+            ModificarBloqueo();
+        }
+
+        private void ModificarBloqueo() {
+            var estaBloqueado = Cursor.lockState == CursorLockMode.Locked;
+
+            Cursor.lockState = estaBloqueado ? CursorLockMode.None : CursorLockMode.Locked;
+        }
+    }
+}
