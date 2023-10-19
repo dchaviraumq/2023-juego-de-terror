1091c18f035cc727ac0ccc033f10e6bbc04509d7
Daniel Chavira <prof.d.chavira@umq.maristas.edu.mx>
2023-10-19 12:09:14 -0600

    Added on and off materials so the light bulbs don't glow in the dark.

--- Assets/Scripts/Environment/LightSwitch.cs
+++ Assets/Scripts/Environment/LightSwitch.cs
@@ -6,24 +6,43 @@ namespace Environment
 {
     public class LightSwitch : MonoBehaviour, IInteractable
     {
-        [SerializeField] private Light[] lights;
-        [SerializeField] private bool    isOn;
+        [SerializeField] private Light[]        lights;
+        [SerializeField] private MeshRenderer[] lightBulbs;
+
+
+        [SerializeField] private Material onMaterial;
+        [SerializeField] private Material offMaterial;
+
+        [SerializeField] private bool isOn;
 
         [HideInInspector] [SerializeField] private string interactionText;
 
         public string InteractionText => isOn ? "Turn Off" : "Turn On";
 
-        private void Awake() => ToggleLights();
+        private void Awake() => Toggle();
 
         public void Interact() {
             isOn = !isOn;
+            Toggle();
+        }
+
+        private void Toggle() {
             ToggleLights();
+            ToggleMaterial();
         }
 
         private void ToggleLights() {
             foreach (var theLight in lights)
             {
                 theLight.enabled = isOn;
+            }
+        }
+
+        private void ToggleMaterial() {
+            var mat = isOn ? onMaterial : offMaterial;
+            foreach (var lightBulb in lightBulbs)
+            {
+                lightBulb.material = mat;
             }
         }
     }
