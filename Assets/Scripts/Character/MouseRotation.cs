f3ad1e75976e5fd9ef5850deb62536f61d305880
Daniel Chavira <prof.d.chavira@umq.maristas.edu.mx>
2023-10-19 10:27:55 -0600

    Player scripts: Movement & Mouse rotation

---
+++ Assets/Scripts/Character/MouseRotation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Character
+{
+    public class MouseRotation : MonoBehaviour
+    {
+        [SerializeField] private float mouseSensitivityX = 2f;
+        [SerializeField] private bool  invertX           = false;
+
+        [SerializeField] private float mouseSensitivityY = 2f;
+        [SerializeField] private bool  invertY           = false;
+        [SerializeField] private float yClamp            = 45f;
+
+        private int InvertX => invertX ? -1 : 1;
+        private int InvertY => invertY ? -1 : 1;
+
+        private Vector2 _turn = Vector2.zero;
+
+        private void Update() {
+            _turn.x += Input.GetAxis("Mouse X") * InvertX * mouseSensitivityX;
+            _turn.y += -Input.GetAxis("Mouse Y") * InvertY * mouseSensitivityY;
+            
+            _turn.y = Mathf.Clamp(_turn.y, -yClamp, yClamp);
+
+            transform.localRotation = Quaternion.Euler(_turn.y, _turn.x, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace Character
+{
+    [SelectionBase]
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float speed        = 5f;
+        [SerializeField] private float jumpStrength = 4f;
+        [SerializeField] private float gravity      = Physics.gravity.y;
+
+
+        private CharacterController _characterController;
+
+        private Vector3 _moveDirection;
+
+        private void Awake() {
+            _characterController = GetComponent<CharacterController>();
+        }
+
+        private void Update() {
+            var x    = Input.GetAxis("Horizontal");
+            var jump = Input.GetButtonDown("Jump");
+            var z    = Input.GetAxis("Vertical");
+
+            var zMovement = z * speed;
+            var xMovement = x * speed;
+            var yMovement = _moveDirection.y;
+
+            // Jump or apply gravity
+            if (jump && _characterController.isGrounded)
+            {
+                yMovement = jumpStrength;
+            }
+            else if (!_characterController.isGrounded)
+            {
+                yMovement += gravity * Time.deltaTime;
+            }
+
+            _moveDirection   = new Vector3(xMovement, 0, zMovement);
+            _moveDirection   = transform.TransformDirection(_moveDirection);
+            _moveDirection.y = yMovement;
+
+            _characterController.Move(_moveDirection * Time.deltaTime);
+        }
+    }
+}
