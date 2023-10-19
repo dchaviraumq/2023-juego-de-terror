6b1e3d45f497f002d746f563c0f582ad75639caa
Daniel Chavira <prof.d.chavira@umq.maristas.edu.mx>
2023-10-19 16:47:56 -0600

    Mover la cámara con el mpuse

---
+++ Assets/Scripts/Jugador/CamaraJugador.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Jugador
+{
+    public class CamaraJugador : MonoBehaviour
+    {
+        [SerializeField] private float sensibilidadX = 1f;
+        [SerializeField] private float sensibilidadY = 1f;
+
+        [SerializeField] private float limiteY = 45f;
+
+        private Vector2 _rotacion = Vector2.zero;
+
+        private void Update() {
+            _rotacion.x += Input.GetAxis("Mouse X");
+            _rotacion.y -= Input.GetAxis("Mouse Y");
+
+            _rotacion.x *= sensibilidadX;
+            _rotacion.y *= sensibilidadY;
+
+            _rotacion.y = Mathf.Clamp(_rotacion.y, -limiteY, limiteY);
+
+            transform.localRotation = Quaternion.Euler(_rotacion.y, _rotacion.x, 0);
+        }
+    }
+}
