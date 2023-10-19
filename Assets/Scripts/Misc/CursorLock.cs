003264bec1adc835757d0555840cf17ae292d758
Daniel Chavira <prof.d.chavira@umq.maristas.edu.mx>
2023-10-19 10:21:05 -0600

    Cursor lock script

---
+++ Assets/Scripts/Misc/CursorLock.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Misc
+{
+    public class CursorLock : MonoBehaviour
+    {
+        [SerializeField] private KeyCode cursorLockKey = KeyCode.Escape;
+
+        private void Awake() {
+            ToggleCursorLock();
+        }
+
+        private void Update() {
+            if (Input.GetKeyDown(cursorLockKey))
+            {
+                ToggleCursorLock();
+            }
+        }
+
+        private void ToggleCursorLock() {
+            var isLocked = Cursor.lockState == CursorLockMode.Locked;
+            Cursor.lockState = isLocked ? CursorLockMode.None : CursorLockMode.Locked;
+        }
+    }
+}
