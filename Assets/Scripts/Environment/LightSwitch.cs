7ffc8f3a90fa6e1da057c86902c2407c1acdbbff
Daniel Chavira <prof.d.chavira@umq.maristas.edu.mx>
2023-10-19 11:59:06 -0600

    Light Switch done!

---
+++ Assets/Scripts/Environment/LightSwitch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Environment
+{
+    public class LightSwitch : MonoBehaviour, IInteractable
+    {
+        [SerializeField] private Light[] lights;
+        [SerializeField] private bool    isOn;
+
+        [HideInInspector] [SerializeField] private string interactionText;
+
+        public string InteractionText => isOn ? "Turn Off" : "Turn On";
+
+        private void Awake() => ToggleLights();
+
+        public void Interact() {
+            isOn = !isOn;
+            ToggleLights();
+        }
+
+        private void ToggleLights() {
+            foreach (var theLight in lights)
+            {
+                theLight.enabled = isOn;
+            }
+        }
+    }
+}
