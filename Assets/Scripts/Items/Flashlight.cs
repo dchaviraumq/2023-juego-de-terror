be44065fed94e6122f4fa6c8cb54b5666b3fefbb
Daniel Chavira <prof.d.chavira@umq.maristas.edu.mx>
2023-10-19 13:13:06 -0600

    Flashlight Done!

---
+++ Assets/Scripts/Items/Flashlight.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Items
+{
+    public class Flashlight : MonoBehaviour
+    {
+        [SerializeField] private KeyCode flashlightKey = KeyCode.F;
+
+        [SerializeField] private Light        lightBulb;
+        [SerializeField] private MeshRenderer glass;
+
+        [SerializeField] private bool isOn;
+
+        [SerializeField] private Material onMaterial;
+        [SerializeField] private Material offMaterial;
+
+        private void Awake() {
+            ToggleFlashlight();
+        }
+
+        private void Update() {
+            if (!Input.GetKeyDown(flashlightKey)) return;
+            
+            isOn = !isOn;
+            ToggleFlashlight();
+        }
+
+        private void ToggleFlashlight() {
+            lightBulb.enabled = isOn;
+            var mat = isOn ? onMaterial : offMaterial;
+            glass.material = mat;
+        }
+    }
+}
