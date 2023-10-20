d4cba643067dde2795edb67fa85efb1202d3804c
Daniel Chavira <prof.d.chavira@umq.maristas.edu.mx>
2023-10-19 18:21:20 -0600

    Foco

---
+++ Assets/Scripts/Contratos/I_Interactuable.cs
@@ -0,0 +1,7 @@
+﻿namespace Scripts.Contratos
+{
+    public interface I_Interactuable
+    {
+        public void Interactuar();
+    }
+}
---
+++ Assets/Scripts/Objetos/ControlLuces.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Scripts.Contratos;
+using UnityEngine;
+
+namespace Scripts.Objetos
+{
+    public class ControlLuces : MonoBehaviour, I_Interactuable
+    {
+        [SerializeField] private Light[]        luces;
+        [SerializeField] private MeshRenderer[] focos;
+
+        [SerializeField] private Material luzPrendida;
+        [SerializeField] private Material luzApagada;
+
+        [SerializeField] private bool encendido;
+
+        private void Awake() {
+            AlternarEncendido();
+        }
+
+        public void Interactuar() {
+            encendido = !encendido;
+            AlternarEncendido();
+        }
+
+        private void AlternarEncendido() {
+            foreach (var luz in luces)
+            {
+                luz.enabled = encendido;
+            }
+
+            var material = encendido ? luzPrendida : luzApagada;
+            foreach (var foco in focos)
+            {
+                foco.material = material;
+            }
+        }
+    }
+}
