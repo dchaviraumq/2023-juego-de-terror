63d7ec6e323851a4c9414223537d4ea564ee60ad
Daniel Chavira <prof.d.chavira@umq.maristas.edu.mx>
2023-10-26 11:32:42 -0600

    Door animation

---
+++ Assets/Scripts/Objetos/Puerta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Scripts.Contratos;
+using UnityEngine;
+
+namespace Objetos
+{
+    public class Puerta : MonoBehaviour, I_Interactuable
+    {
+        public string TextoDeInteraccion => abierta ? "Cerrar" : "Abrir";
+
+        [SerializeField] private bool abierta;
+
+        private Animator _animator;
+
+        private static readonly int AbrirTrigger  = Animator.StringToHash("Open");
+        private static readonly int CerrarTrigger = Animator.StringToHash("Close");
+
+        private void Start() {
+            _animator = GetComponent<Animator>();
+        }
+
+        public void Interactuar() {
+            abierta = !abierta;
+            if (abierta)
+            {
+                _animator.ResetTrigger(CerrarTrigger);
+                _animator.SetTrigger(AbrirTrigger);
+            }
+            else
+            {
+                _animator.ResetTrigger(AbrirTrigger);
+                _animator.SetTrigger(CerrarTrigger);
+            }
+        }
+    }
+}
