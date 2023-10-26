1b13bc6aface1be3700bc38bc121708bc2691ce5
Daniel Chavira <prof.d.chavira@umq.maristas.edu.mx>
2023-10-26 08:15:06 -0600

    Interaction UI component

---
+++ Assets/Scripts/Jugador/MIsc/PanelDeInteraccion.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Jugador.MIsc
+{
+    public class PanelDeInteraccion : MonoBehaviour
+    {
+        [SerializeField] private GameObject fondo;
+        [SerializeField] private TMP_Text   etiqueta;
+
+        public void Activar(string texto) {
+            fondo.SetActive(true);
+            etiqueta.text = texto;
+        }
+
+
+        public void Desactivar() {
+            etiqueta.text = string.Empty;
+            fondo.SetActive(false);
+        }
+    }
+}
