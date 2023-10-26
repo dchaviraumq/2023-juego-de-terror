bce0bd4e6364e193dbe60089b743683cbd889162
Daniel Chavira <prof.d.chavira@umq.maristas.edu.mx>
2023-10-26 09:00:57 -0600

    Added Pickup Script

---
+++ Assets/Scripts/Objetos/Common/Pickup.cs
@@ -0,0 +1,25 @@
+﻿using Scripts.Contratos;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Objetos.Common
+{
+    public class Pickup : MonoBehaviour, I_Interactuable
+    {
+        [TextArea] 
+        [SerializeField] private string textoDeInteraccion;
+
+        [Space(5)] 
+        [SerializeField] private bool desapareceAlInteractuar = true;
+
+        [Space(5)] 
+        [SerializeField] private UnityEvent onInteract;
+
+        public string TextoDeInteraccion => textoDeInteraccion;
+
+        public void Interactuar() {
+            onInteract?.Invoke();
+            gameObject.SetActive(!desapareceAlInteractuar);
+        }
+    }
+}
