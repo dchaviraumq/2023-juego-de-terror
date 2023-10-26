44af48d3237e8be375ed5ce5c168b18a75bd6c5e
Daniel Chavira <prof.d.chavira@umq.maristas.edu.mx>
2023-10-26 08:42:23 -0600

    Flashlight prefab & script

---
+++ Assets/Scripts/Objetos/Linterna.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Objetos
+{
+    public class Linterna : MonoBehaviour
+    {
+        [SerializeField] private KeyCode accion = KeyCode.F;
+
+        [SerializeField] private Light        luz;
+        [SerializeField] private MeshRenderer vidrio;
+
+        [SerializeField] private Material luzPrendida;
+        [SerializeField] private Material luzApagada;
+
+        [SerializeField] private bool encendida;
+
+        private void Start() {
+            AlternarEncendido();
+        }
+
+        private void Update() {
+            if (!Input.GetKeyDown(accion)) return;
+            encendida = !encendida;
+
+            AlternarEncendido();
+        }
+
+        private void AlternarEncendido() {
+            luz.enabled = encendida;
+            var material = encendida ? luzPrendida : luzApagada;
+            vidrio.material = material;
+        }
+    }
+}
