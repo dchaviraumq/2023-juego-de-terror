2d4eb87b9a196148839ff1d7016d6573e6854391
Daniel Chavira <prof.d.chavira@umq.maristas.edu.mx>
2023-10-26 12:57:09 -0600

    Clase para Interaccion del jugador

---
+++ Assets/Scripts/Jugador/InteraccionDelJugador.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Scripts.Contratos;
+using UnityEngine;
+
+namespace Jugador
+{
+    public class InteraccionDelJugador : MonoBehaviour
+    {
+        [SerializeField] private KeyCode accion = KeyCode.E;
+
+        private I_Interactuable _interactuable;
+
+        private void Update() {
+            if (!Input.GetKeyDown(accion)) return;
+            if (_interactuable == null) return;
+
+            _interactuable.Interactuar();
+        }
+
+        private void OnTriggerStay(Collider other) {
+            _interactuable = other.gameObject.GetComponent<I_Interactuable>();
+        }
+
+        private void OnTriggerExit(Collider other) {
+            _interactuable = null;
+        }
+    }
+}
