c388cbf9c3900eb50c2d3fbebc86634139edb211
Daniel Chavira <prof.d.chavira@umq.maristas.edu.mx>
2023-11-09 17:39:25 -0600

    Puerta de abre y cierra

---
+++ Assets/Scripts/Objetos/Puerta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Scripts.Contratos;
+using UnityEngine;
+
+namespace Objetos
+{
+    public class Puerta : MonoBehaviour, I_Interactuable
+    {
+        public string TextoDeInteraccion => abierta ? "Cerrar" : "Abrir";
+
+        [SerializeField] private bool abierta;
+
+        [SerializeField] private Animator _animator;
+
+        public void Interactuar() {
+            abierta = !abierta;
+            if (abierta)
+            {
+                Abrir();
+            }
+            else
+            {
+                Cerrar();   
+            }
+        }
+
+        private void Abrir() {
+            _animator.ResetTrigger("Close");
+            _animator.SetTrigger("Open");
+        }
+
+        private void Cerrar() {
+            _animator.ResetTrigger("Open");
+            _animator.SetTrigger("Close");
+        }
+    }
+}
